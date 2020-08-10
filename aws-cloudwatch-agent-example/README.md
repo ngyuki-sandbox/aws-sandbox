05f115d2f28050d07f291fc5f56886ff8b737a76
Toshiyuki Goto <ngyuki.jp@gmail.com>
2020-08-10 12:15:13 +0900

    [aws-cloudwatch-agent-example]

---
+++ aws-cloudwatch-agent-example/README.md
@@ -0,0 +1,6 @@
+# CloudWatch Agent を試す
+
+- userdata/
+    - CloudWatch Agent の設定ファイルを EC2 ユーザーデータに入れる例
+- ssm/
+    - CloudWatch Agent の設定ファイルを SSM Parameter Store に入れる例
---
+++ aws-cloudwatch-agent-example/userdata/cloudwatch-agent.tf
@@ -0,0 +1,126 @@
+////////////////////////////////////////////////////////////////////////////////
+// AWS
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Variable
+
+variable "key_name" {}
+
+////////////////////////////////////////////////////////////////////////////////
+// IAM
+
+resource "aws_iam_role" "ec2" {
+  name = "hello-cwagent"
+
+  assume_role_policy = <<EOS
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOS
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_CloudWatchAgentServerPolicy" {
+  role       = "${aws_iam_role.ec2.name}"
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "hello-cwagent"
+  role = "${aws_iam_role.ec2.name}"
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// VPC
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_security_group" "default" {
+  name = "default"
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// EC2
+
+data "aws_ami" "app" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("user_data.yaml")}"
+
+  vars = {
+    amazon_cloudwatch_agent = "${file("amazon-cloudwatch-agent.json")}"
+  }
+}
+
+resource "aws_instance" "app" {
+  count                       = 2
+  ami                         = "${data.aws_ami.app.id}"
+  instance_type               = "t2.nano"
+  key_name                    = "${var.key_name}"
+  iam_instance_profile        = "${aws_iam_instance_profile.ec2.name}"
+  subnet_id                   = "${data.aws_subnet_ids.default.ids[count.index]}"
+  vpc_security_group_ids      = ["${data.aws_security_group.default.id}"]
+  associate_public_ip_address = true
+
+  user_data = "${data.template_file.user_data.rendered}"
+
+  tags {
+    Name = "hello-cwagent"
+  }
+
+  root_block_device {
+    volume_size           = 8
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+}
+
+output "aws_instance.app.public_ip" {
+  value = "${aws_instance.app.*.public_ip}"
+}
